1fb960b2eed36c5c27d27edd307e634d7815cdf5
ShreyasP20 <luciferjones2015@gmail.com>
2024-03-24 15:43:34 +0530

    Final_Changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-import openai
 import pathlib
 import textwrap
 import json
@@ -12,32 +11,16 @@ def to_markdown(text):
 
 app = Flask(__name__)
 genai.configure(api_key='AIzaSyB2ykTpIgDjPe59LxWAIw_6QYjLdwrmNAA')
-# Set up OpenAI API credentials
-#openai.api_key = 'AIzaSyB2ykTpIgDjPe59LxWAIw_6QYjLdwrmNAA'
 model = genai.GenerativeModel('gemini-pro')
 
-# Define the default route to return the index.html file
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Define the /api route to handle POST requests
 @app.route("/api", methods=["POST"])
 def api():
-    # Get the message from the POST request
     message = request.json.get("message")
-    # Send the message to OpenAI's API and receive the response
-    
-    
-    # completion = openai.chat.completions.create(
-    # model="gpt-3.5-turbo",
-    # messages=[
-    #     {"role": "user", "content": message}
-    # ]
-    # )
-    print(message)
     response = model.generate_content(message)
-    print(response.text)
     response_json = json.dumps({"content":response.text})
     if response!=None:
         return response_json
